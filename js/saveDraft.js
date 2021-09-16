3fafac998818daa72fa4489a73f8928a924ebd75
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2021-09-16 19:01:51 +0200

    toogleDarkMode update

--- js/saveDraft.js
+++ js/saveDraft.js
@@ -17,23 +17,126 @@ invoice.invoiceDate = document.getElementById('invoiceDate').value;
 invoice.paymentTerms = document.getElementById('paymentTerms').value;
 invoice.projectDescription = document.getElementById('projectDescription').value;
 
+let itemList1 = {
+    itemName: 'Lego',
+    qty: 1,
+    price: 156,
+    total: 156,
+}
+
+const invoice1 = {
+    id: generateID(),
+    address: 'Pri Hornade 380',
+    city: 'Kosice',
+    postCode: 24415,
+    country: 'Slovakia',
+
+    clientName: 'Lukas',
+    clientEmail: 'lukas.bujdos@gmail.com',
+    clientAddress: 'Uzhorodska 18',
+    clientCity: 'Kosice',
+    clientPostCode: 24415,
+    clientCountry: 'Czech',
+    InvoiceDate: '27.4.2018',
+    paymentTerms: 'Net 1 day',
+    projectDescription: 'Programmer',
+    itemLists: [itemList1],
+};
+
+const invoice2 = {
+    id: generateID(),
+    address: 'Pri Hornade 380',
+    city: 'Kosice',
+    postCode: 24415,
+    country: 'Slovakia',
+
+    clientName: 'Lukas',
+    clientEmail: 'lukas.bujdos@gmail.com',
+    clientAddress: 'Uzhorodska 18',
+    clientCity: 'Kosice',
+    clientPostCode: 24415,
+    clientCountry: 'Czech',
+    InvoiceDate: '27.4.2018',
+    paymentTerms: 'Net 1 day',
+    projectDescription: 'Programmer',
+    itemLists: [itemList1],
+};
+
+const invoice3 = {
+    id: generateID(),
+    address: 'Pri Hornade 380',
+    city: 'Kosice',
+    postCode: 24415,
+    country: 'Slovakia',
+
+    clientName: 'Lukas',
+    clientEmail: 'lukas.bujdos@gmail.com',
+    clientAddress: 'Uzhorodska 18',
+    clientCity: 'Kosice',
+    clientPostCode: 24415,
+    clientCountry: 'Czech',
+    InvoiceDate: '27.4.2018',
+    paymentTerms: 'Net 1 day',
+    projectDescription: 'Programmer',
+    itemLists: [itemList1],
+};
+
+const invoice4 = {
+    id: generateID(),
+    address: 'Pri Hornade 380',
+    city: 'Kosice',
+    postCode: 24415,
+    country: 'Slovakia',
+
+    clientName: 'Lukas',
+    clientEmail: 'lukas.bujdos@gmail.com',
+    clientAddress: 'Uzhorodska 18',
+    clientCity: 'Kosice',
+    clientPostCode: 24415,
+    clientCountry: 'Czech',
+    InvoiceDate: '27.4.2018',
+    paymentTerms: 'Net 1 day',
+    projectDescription: 'Programmer',
+    itemLists: [itemList1],
+};
+
+const invoicesArray = [invoice1, invoice2, invoice3, invoice4];
+
+const browseBtn = document.querySelector('#browse');
+
+browseBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('#createAccount').style.display = 'none';
+    document.querySelector('#body-view').style.display = 'none';
+    document.querySelector('.sidepanel').style.display = 'flex';
+    document.querySelector('#main-view ').style.display = 'flex';
+});
+
 function renderInvoice() {
+    const invoicesArray = document.getElementById('invoices-array');
+    const div = document.createElement("div");
 
-    document.getElementById('invoices-array').innerHTML =
+    div.innerHTML =
 
-        `<div class="invoices-element">
-            <div>${generateID()}</div>
-            <div class="grey">${invoice.invoiceDate}</div>
-            <div class="grey">${invoice.clientName}</div>
-            <div class="bold">120</div>
-            <div class="right">
-            <div class="status-value-udpate-draft"><span class="status-dot-draft">&#11044;</span> Draft</div>
-            <div><img class="arrow-right" src="/assets/icon-arrow-right.svg" alt="Arrow right"></div>
+        `<a class="invoices-link" href="/invoice.html">
+            <div class="invoices-element">
+                    <div>${generateID()}</div>
+                    <div class="grey">${invoice.invoiceDate}</div>
+                    <div class="grey">${invoice.clientName}</div>
+                    <div class="bold">120</div>
+                    <div class="right">
+                    <div class="status-value-udpate-draft"><span class="status-dot-draft">&#11044;</span> Draft</div>
+                    <div><img class="arrow-right" src="/assets/icon-arrow-right.svg" alt="Arrow right"></div>
+                    </div>
             </div>
-        </div>`;
+        </a>`;
+
+    invoicesArray.append(div);
+    console.log(localStorage.setItem("invoice", div));
 }
 
 function saveDraft() {
+
     let drafts = [];
 
     document.getElementById('empty').style.display = 'none';
@@ -47,7 +150,12 @@ function saveDraft() {
     document.getElementById('new').style.display = 'none';
 
 
+
     renderInvoice();
+}
+
+function discard() {
+    document.getElementById('new').style.display = 'none';
 }
 
 function generateID() {
@@ -60,25 +168,6 @@ function generateID() {
     return id;
 }
 
-// function check() {
-//     let valid = true;
-//     let error = "";
-//     let field = "";
-
-//     field = document.getElementById('clientName');
-//     error = document.getElementById('cname');
-
-//     if (!field.checkValidity()) {
-//         valid = false;
-//         field.classList.add("err");
-//         error.innerHTML = "Name must be 2-4 characters\r\n";
-//     } else {
-//         field.classList.remove("err");
-//         error.innerHTML = "";
-//     }
-
-//     return valid;
-// }
 
 
 
--- js/toggleDarkMode.js
+++ js/toggleDarkMode.js
@@ -12,111 +12,41 @@ function toggleDarkMode() {
 
     // Dark Mode 1
 
-    let element = document.getElementsByClassName('main-view');
-    for (i = 0; i < element.length; i++) {
-        element[i].classList.toggle('dark-mode-1');
-    }
-
-    let element1 = document.getElementsByClassName('modal-content-new');
-    for (i = 0; i < element1.length; i++) {
-        element1[i].classList.toggle('dark-mode-1');
-    }
-
-    let element2 = document.getElementsByClassName('modal-new-buttons');
-    for (i = 0; i < element2.length; i++) {
-        element2[i].classList.toggle('dark-mode-1');
-    }
-
-    let element3 = document.getElementsByClassName('modal-edit-buttons');
-    for (i = 0; i < element3.length; i++) {
-        element3[i].classList.toggle('dark-mode-1');
-    }
-
-    let element4 = document.getElementsByClassName('modal-content-edit');
-    for (i = 0; i < element4.length; i++) {
-        element4[i].classList.toggle('dark-mode-1');
-    }
-
-    let elementFilterStatus = document.getElementsByClassName('filter-status');
-    for (i = 0; i < elementFilterStatus.length; i++) {
-        elementFilterStatus[i].classList.toggle('dark-mode-1');
-    }
+    document.querySelector('.main-view').classList.toggle('dark-mode-1');
+    document.querySelector('.modal-content-new').classList.toggle('dark-mode-1');
+    document.querySelector('.modal-new-buttons').classList.toggle('dark-mode-1');
+    document.querySelector('.modal-edit-buttons').classList.toggle('dark-mode-1');
+    document.querySelector('.modal-content-edit').classList.toggle('dark-mode-1');
+    document.querySelector('.filter-status').classList.toggle('dark-mode-1');
 
     // Dark Mode 2
 
-    let element5 = document.getElementsByClassName('input');
-    for (i = 0; i < element5.length; i++) {
-        element5[i].classList.toggle('dark-mode-2');
-    }
-
-    let elementInput = document.getElementsByClassName('status');
-    for (i = 0; i < elementInput.length; i++) {
-        elementInput[i].classList.toggle('dark-mode-2');
-    }
-
-    let elementDetails = document.getElementsByClassName('details');
-    for (i = 0; i < elementDetails.length; i++) {
-        elementDetails[i].classList.toggle('dark-mode-2');
-    }
-
-    let elementInvoice = document.getElementsByClassName('invoices-element');
-    for (i = 0; i < elementInvoice.length; i++) {
-        elementInvoice[i].classList.toggle('dark-mode-2');
-    }
+    document.querySelector('.input').classList.toggle('dark-mode-2');
+    document.querySelector('.status').classList.toggle('dark-mode-2');
+    document.querySelector('.details').classList.toggle('dark-mode-2');
+    document.querySelector('.invoices-element').classList.toggle('dark-mode-2');
 
     // Dark Mode 3
 
-    let element6 = document.getElementsByClassName('modal-container-edit');
-    for (i = 0; i < element6.length; i++) {
-        element6[i].classList.toggle('dark-mode-3');
-    }
-
-    let element7 = document.getElementsByClassName('modal-container-new');
-    for (i = 0; i < element7.length; i++) {
-        element7[i].classList.toggle('dark-mode-3');
-    }
+    document.querySelector('.modal-container-edit').classList.toggle('dark-mode-3');
+    document.querySelector('.modal-container-new').classList.toggle('dark-mode-3');
 
     // Dark Mode 4
 
-    let element8 = document.getElementsByClassName('amount');
-    for (i = 0; i < element8.length; i++) {
-        element8[i].classList.toggle('dark-mode-4');
-    }
+    document.querySelector('.amount').classList.toggle('dark-mode-4');
 
     // Dark Mode 5
 
-    let elementInvoiceTable = document.getElementsByClassName('invoice-table');
-    for (i = 0; i < elementInvoiceTable.length; i++) {
-        elementInvoiceTable[i].classList.toggle('dark-mode-5');
-    }
-
-    let elementBtnEdit = document.getElementsByClassName('edit');
-    for (i = 0; i < elementBtnEdit.length; i++) {
-        elementBtnEdit[i].classList.toggle('dark-mode-5');
-    }
+    document.querySelector('.invoice-table').classList.toggle('dark-mode-5');
+    document.querySelector('.edit').classList.toggle('dark-mode-5');
 
     // Dark Mode 6
 
-    let elementBack = document.getElementsByClassName('back-paragraph');
-    for (i = 0; i < elementBack.length; i++) {
-        elementBack[i].classList.toggle('dark-mode-6');
-    }
-
-    let elementPlaceholder = document.getElementsByClassName('whitePlaceholder');
-    for (i = 0; i < elementPlaceholder.length; i++) {
-        elementPlaceholder[i].classList.toggle('dark-mode-6');
-    }
+    document.querySelector('.back-paragraph').classList.toggle('dark-mode-6');
+    document.querySelector('.whitePlaceholder').classList.toggle('dark-mode-6');
 
     // Dark Mode 7
 
-    let elementPending = document.getElementsByClassName('status-value-update');
-    for (i = 0; i < elementPending.length; i++) {
-        elementPending[i].classList.toggle('dark-mode-7');
-    }
-
-    let elementPaid = document.getElementsByClassName('status-value-update-paid');
-    for (i = 0; i < elementPaid.length; i++) {
-        elementPaid[i].classList.toggle('dark-mode-7');
-    }
-
+    document.querySelector('.status-value-update').classList.toggle('dark-mode-7');
+    document.querySelector('.status-value-update-paid').classList.toggle('dark-mode-7');
 }
